f0f2368e7d7e0656719d11742cd9c90313f48090
spanozzo <panozzostefano@gmail.com>
2018-07-03 11:30:58 +0200

    .

--- HTML/admin/adminPrenotazioni.php
+++ HTML/admin/adminPrenotazioni.php
@@ -1,6 +1,5 @@
 <?php require_once __DIR__ . "/../../php/connection.php"; ?>
 <?php require_once('../../validation/validator.php'); ?>
-<!-- <?php require_once('../../scripts/script.js'); ?> -->
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="it" lang="it">
@@ -25,13 +24,12 @@ if (!isAdmin()) {
     <meta http-equiv="content-type" content="text/html; charset=utf-8" />
     <meta name="description" content="Pagina di gestione delle prenotazioni" />
     <meta name="author" content="Tecwweb&amp;Pastorizia" />
-    <meta name="keywords" content="agricoltura, azienda, agricola, grano, duro, biologico, HTML, CSS, JavaScript, MySQL, informatica">
+    <meta name="keywords" content="agricoltura, azienda, agricola, grano, duro, biologico, HTML, CSS, JavaScript, MySQL, informatica"/>
     <link rel="icon" type="image/png" href="../../images/icon/favicon-32x32.png" sizes="32x32" />
     <link rel="icon" type="image/png" href="../../images/icon/favicon-16x16.png" sizes="16x16" />
     <link href="../../css/administrator.css" rel="stylesheet" type="text/css" media="handheld, screen" />
     <!-- <link href="../css/small.css" type="text/css" rel="stylesheet" media="handheld, screen and (max-width:480px),only screen and (max-device-width:480px)" /> -->
     <!-- <link href="../css/print.css" type="text/css" rel="stylesheet" media="print" /> -->
-    <script src="../../scripts/script.js" type="text/javascript" charset="utf-8"></script>
   </head>
 
   <body>
@@ -123,7 +121,6 @@ if (!isAdmin()) {
         } else
             echo '<div id="">Non ci sono macchinari registrati! Registrane prima uno.</div>';
         ?>
-          <form method="post">
             <fieldset>
               <legend>Scegli le date della prenotazione:</legend>
               <div>
@@ -139,9 +136,6 @@ if (!isAdmin()) {
               </div>
 
             </fieldset>
-          </form>
-
-
 
               <input type="submit" value="Aggiungi prenotazione" name="submit"/>
               
